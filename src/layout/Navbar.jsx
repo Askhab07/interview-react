11d26840d4514e9614d11506503802a4ff13dbc2
Askhab <akhtaevaskhab07@mail.ru>
2024-12-06 13:57:05 +0300

    navlink

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 import { HomeIcon, UserCircleIcon, CommandLineIcon, AcademicCapIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
   return (
     <nav className='fixed bottom-0 dark:bg-slate-800 bg-white w-full text-xs border-t-2 h-20'>
       <ul className='flex justify-between items-center h-16'>
-        <li><Link className='flex flex-col gap-1 items-center w-20 dark:text-white' to='/'><HomeIcon className='size-6 dark:text-white text-blue-500'/>Главная</Link></li>
-        <li><Link className='flex flex-col gap-1 items-center w-20 dark:text-white' to='/education'><AcademicCapIcon className='size-6 dark:text-white text-blue-500'/>Обучение</Link></li>
-        <li><Link className='flex flex-col gap-1 items-center w-20 dark:text-white' to='/inter'><CommandLineIcon className='size-6 dark:text-white text-blue-500'/>Собеседование</Link></li>
-        <li><Link className='flex flex-col gap-1 items-center w-20 dark:text-white' to='/profile'><UserCircleIcon className='size-6 dark:text-white text-blue-500'/>Админ</Link></li>
+        <li><NavLink className={({isActive}) => `flex flex-col gap-1 items-center w-20 ${[isActive ? 'text-blue-500' : '']} dark:text-white`} to='/'><HomeIcon className='size-6 dark:text-white '/>Главная</NavLink></li>
+        <li><NavLink className={({isActive}) => `flex flex-col gap-1 items-center w-20 ${[isActive ? 'text-blue-500' : '']} dark:text-white`} to='/education'><AcademicCapIcon className='size-6 dark:text-white'/>Обучение</NavLink></li>
+        <li><NavLink className={({isActive}) => `flex flex-col gap-1 items-center w-20 ${[isActive ? 'text-blue-500' : '']} dark:text-white`} to='/inter'><CommandLineIcon className='size-6 dark:text-white'/>Собеседование</NavLink></li>
+        <li><NavLink className={({isActive}) => `flex flex-col gap-1 items-center w-20 ${[isActive ? 'text-blue-500' : '']} dark:text-white`} to='/profile'><UserCircleIcon className='size-6 dark:text-white'/>Админ</NavLink></li>
       </ul>
     </nav>
   );
